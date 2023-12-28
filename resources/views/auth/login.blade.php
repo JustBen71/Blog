8244b2c14fa7066e455f4dc6c9563cabb67a809e
MV <mathilde.vollet.auditeur@lecnam.net>
2023-12-28 22:35:14 +0100

    ajout message si pas d'articles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,8 +4,8 @@
 
 @section('contenu')
     <form method="POST" class="vstack gap-3">
-            <div class="row">
-                <div class="col-6">
+            <div class="row justify-content-center">
+                <div class="col-4">
                 @csrf
                 <div class="form-group">
                     <label for="mailUtilisateur">Email :</label>
@@ -18,15 +18,15 @@
 
                 </div>
             </div>
-            <div class="row">
-                <div class="col-6">
+            <div class="row justify-content-center">
+                <div class="col-4">
                     <div class="form-group">
                         <label for="password">Mot de passe :</label>
                         <input type="password" class="form-control" name="password" required>
                     </div>
                 </div>
             </div>
-            <div class="row mt-2">
+            <div class="row mt-2 text-center">
                 <div class="col-12">
                     <button class="btn btn-primary" type="submit">Se connecter</button>
                 </div>
